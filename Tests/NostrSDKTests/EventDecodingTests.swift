d0625429580603664d0f2ded88b01e46f14ceddc
Bryan Montz <bryan.montz@lifechurch.tv>
2023-05-23 06:52:07 -0500

    add test to decode a kind 0 set_metadata event

--- Tests/NostrSDKTests/EventDecodingTests.swift
+++ Tests/NostrSDKTests/EventDecodingTests.swift
@@ -10,6 +10,32 @@ import XCTest
 
 final class EventDecodingTests: XCTestCase {
 
+    func testDecodeSetMetadata() throws {
+        let json = """
+              {
+                "content": "{\\"banner\\":\\"https://nostr.build/i/nostr.build_90a51a2e50c9f42288260d01b3a2a4a1c7a9df085423abad7809e76429da7cdc.gif\\",\\"website\\":\\"https://primal.net/cameri\\",\\"reactions\\":true,\\"damus_donation\\":100,\\"picture\\":\\"https://nostr.build/i/9396d5cd901304726883aea7363543f121e1d53964dd3149cadecd802608aebe.jpg\\",\\"nip05\\":\\"cameri@elder.nostr.land\\",\\"lud16\\":\\"cameri@getalby.com\\",\\"display_name\\":\\"Cameri 🦦⚡️\\",\\"about\\":\\"@HodlWithLedn. All opinions are my own.\\nBitcoiner class of 2021. Core Nostr Developer. Author of Nostream. Professional Relay Operator.\\",\\"name\\":\\"cameri\\"}",
+                "created_at": 1684370291,
+                "id": "d214c914b0ab49ec919fa5f60fabf746f421e432d96f941bd2573e4d22e36b51",
+                "kind": 0,
+                "pubkey": "00000000827ffaa94bfea288c3dfce4422c794fbb96625b6b31e9049f729d700",
+                "sig": "7bb7f031fbf41f49eeb44fdfb061bc8d143197d33fae8d29b017709adf2b17c76e78ccb2ee128ee93d0661cad4c626a747d48a178745c94944a693ff31ea7619",
+                "tags": []
+              }
+        """
+
+        let data = try XCTUnwrap(json.data(using: .utf8))
+
+        let event = try JSONDecoder().decode(NostrEvent.self, from: data)
+        
+        XCTAssertEqual(event.id, "d214c914b0ab49ec919fa5f60fabf746f421e432d96f941bd2573e4d22e36b51")
+        XCTAssertEqual(event.pubkey, "00000000827ffaa94bfea288c3dfce4422c794fbb96625b6b31e9049f729d700")
+        XCTAssertEqual(event.createdAt, 1684370291)
+        XCTAssertEqual(event.kind, .setMetadata)
+        XCTAssertEqual(event.tags, [])
+        XCTAssertTrue(event.content.hasPrefix("{\"banner\":\"https://nostr.build/i/nostr.build"))
+        XCTAssertEqual(event.signature, "7bb7f031fbf41f49eeb44fdfb061bc8d143197d33fae8d29b017709adf2b17c76e78ccb2ee128ee93d0661cad4c626a747d48a178745c94944a693ff31ea7619")
+    }
+    
     func testDecodeTextNote() throws {
         let json = """
               {
