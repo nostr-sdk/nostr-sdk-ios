81b3abeddb5486b342a7874d13466ee06f193cd3
Terry Yiu <963907+tyiu@users.noreply.github.com>
2023-05-24 01:37:51 -0400

    Add dependency to secp256k1 and add structs for keys

---
+++ Sources/NostrSDK/String+Hex.swift
@@ -0,0 +1,29 @@
+//
+//  String+Hex.swift
+//
+//
+//  Created by Terry Yiu on 5/28/23.
+//
+
+import Foundation
+
+extension String {
+    
+    var hexDecoded: Data? {
+        guard self.count.isMultiple(of: 2) else { return nil }
+        
+        // https://stackoverflow.com/a/62517446/982195
+        let stringArray = Array(self)
+        var data = Data()
+        for i in stride(from: 0, to: count, by: 2) {
+            let pair = String(stringArray[i]) + String(stringArray[i + 1])
+            if let byteNum = UInt8(pair, radix: 16) {
+                let byte = Data([byteNum])
+                data.append(byte)
+            } else {
+                return nil
+            }
+        }
+        return data
+    }
+}
