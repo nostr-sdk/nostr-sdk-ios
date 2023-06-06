20962675f8889585f11774d5442b4e7cbd95c560
Bryan Montz <bryanmontz@me.com>
2023-06-06 17:52:45 -0500

    minor tweaks to foundational components (#28)

--- Sources/NostrSDK/RelayRequest.swift
+++ Sources/NostrSDK/RelayRequest.swift
@@ -7,34 +7,28 @@
 
 import Foundation
 
-struct RelayRequest {
-
-    static let encoder = JSONEncoder()
-
-    static func close(subscriptionId: String) -> String? {
-        let payload = [AnyEncodable("CLOSE"), AnyEncodable(subscriptionId)]
-        return encode(payload: payload)
-    }
-
-    static func event(_ event: NostrEvent) -> String? {
-        let payload = [AnyEncodable("EVENT"), AnyEncodable(event)]
-        return encode(payload: payload)
-    }
-
-    static func count(subscriptionId: String, filter: Filter) -> String? {
-        let payload = [AnyEncodable("COUNT"), AnyEncodable(subscriptionId), AnyEncodable(filter)]
-        return encode(payload: payload)
-    }
-
-    static func request(subscriptionId: String, filter: Filter) -> String? {
-        let payload = [AnyEncodable("REQ"), AnyEncodable(subscriptionId), AnyEncodable(filter)]
-        return encode(payload: payload)
-    }
-
-    private static func encode(payload: [AnyEncodable]) -> String? {
-        guard let payloadData = try? encoder.encode(payload) else {
+enum RelayRequest {
+    case close(subscriptionId: String)
+    case event(NostrEvent)
+    case count(subscriptionId: String, filter: Filter)
+    case request(subscriptionId: String, filter: Filter)
+    
+    var encoded: String? {
+        let payload: [AnyEncodable]
+        switch self {
+        case .close(let subscriptionId):
+            payload = [AnyEncodable("CLOSE"), AnyEncodable(subscriptionId)]
+        case .event(let event):
+            payload = [AnyEncodable("EVENT"), AnyEncodable(event)]
+        case .count(let subscriptionId, let filter):
+            payload = [AnyEncodable("COUNT"), AnyEncodable(subscriptionId), AnyEncodable(filter)]
+        case .request(let subscriptionId, let filter):
+            payload = [AnyEncodable("REQ"), AnyEncodable(subscriptionId), AnyEncodable(filter)]
+        }
+        
+        guard let data = try? JSONEncoder().encode(payload) else {
             return nil
         }
-        return String(decoding: payloadData, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
+        return String(decoding: data, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
