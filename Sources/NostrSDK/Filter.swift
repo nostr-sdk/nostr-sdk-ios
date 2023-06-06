d2f68e1fa3df7fd8819a15940776165e47949a67
Bryan Montz <bryan.montz@lifechurch.tv>
2023-06-06 06:18:43 -0500

    update Filter to public, add initializer with default nils, add test for all fields

--- Sources/NostrSDK/Filter.swift
+++ Sources/NostrSDK/Filter.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct Filter: Codable {
-    let ids: [String]?
-    let authors: [String]?
-    let kinds: [Int]?
-    let events: [String]?
-    let pubkeys: [String]?
-    let since: Int?
-    let until: Int?
-    let limit: Int?
+public struct Filter: Codable {
+    public let ids: [String]?
+    public let authors: [String]?
+    public let kinds: [Int]?
+    public let events: [String]?
+    public let pubkeys: [String]?
+    public let since: Int?
+    public let until: Int?
+    public let limit: Int?
 
     private enum CodingKeys: String, CodingKey {
         case ids = "ids"
@@ -26,5 +26,16 @@ struct Filter: Codable {
         case since = "since"
         case until = "until"
         case limit = "limit"
+    }
+    
+    public init(ids: [String]? = nil, authors: [String]? = nil, kinds: [Int]? = nil, events: [String]? = nil, pubkeys: [String]? = nil, since: Int? = nil, until: Int? = nil, limit: Int? = nil) {
+        self.ids = ids
+        self.authors = authors
+        self.kinds = kinds
+        self.events = events
+        self.pubkeys = pubkeys
+        self.since = since
+        self.until = until
+        self.limit = limit
     }
 }
