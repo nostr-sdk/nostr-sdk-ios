5ea5040d05a30ce1874b41457eb5167c2e5a5fbe
Bryan Montz <bryan.montz@lifechurch.tv>
2023-06-10 11:27:10 -0500

    fill in Nostr spec documentation for Filter

--- Sources/NostrSDK/Filter.swift
+++ Sources/NostrSDK/Filter.swift
@@ -7,14 +7,32 @@
 
 import Foundation
 
+/// A structure that describes a filter to subscribe to relays with.
+///
+/// > Note: [NIP-01 Specification](https://github.com/nostr-protocol/nips/blob/master/01.md)
 public struct Filter: Codable {
+    /// a list of event ids or prefixes
     public let ids: [String]?
+    
+    /// a list of pubkeys or prefixes, the pubkey of an event must be one of these
     public let authors: [String]?
+    
+    /// a list of a kind numbers
     public let kinds: [Int]?
+    
+    /// a list of event ids that are referenced in an "e" tag
     public let events: [String]?
+    
+    /// a list of pubkeys that are referenced in a "p" tag
     public let pubkeys: [String]?
+    
+    /// an integer unix timestamp, events must be newer than this to pass
     public let since: Int?
+    
+    /// an integer unix timestamp, events must be older than this to pass
     public let until: Int?
+    
+    /// maximum number of events to be returned in the initial query
     public let limit: Int?
 
     private enum CodingKeys: String, CodingKey {
@@ -33,6 +51,14 @@ public struct Filter: Codable {
     /// - Parameters:
     ///   - ids: a list of event ids or prefixes
     ///   - authors: a list of pubkeys or prefixes, the pubkey of an event must be one of these
+    ///   - kinds: a list of a kind numbers
+    ///   - events: a list of event ids that are referenced in an "e" tag
+    ///   - pubkeys: a list of pubkeys that are referenced in a "p" tag
+    ///   - since: an integer unix timestamp, events must be newer than this to pass
+    ///   - until: an integer unix timestamp, events must be older than this to pass
+    ///   - limit: maximum number of events to be returned in the initial query
+    ///
+    ///   > Important: Event ids and pubkeys should be in the 32-byte hexadecimal format, not the `note...` and `npub...` formats
     public init(ids: [String]? = nil, authors: [String]? = nil, kinds: [Int]? = nil, events: [String]? = nil, pubkeys: [String]? = nil, since: Int? = nil, until: Int? = nil, limit: Int? = nil) {
         self.ids = ids
         self.authors = authors
