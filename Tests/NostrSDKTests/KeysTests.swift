d3e653fd90c278fd54afd15939677096f8faf954
klabo <joelklabo@gmail.com>
2023-08-15 06:10:27 -0700

    Prevent Creating Keys with Empty String (#73)

--- Tests/NostrSDKTests/KeysTests.swift
+++ Tests/NostrSDKTests/KeysTests.swift
@@ -14,6 +14,7 @@ final class KeysTests: XCTestCase {
     private let publicKeyHex = "24b98620469cfb06bace51fd7f47d5c60cb663f0f46331fbaf014046712fbb46"
     private let privateKeyNsec = "nsec1t6k4sx3q68dj2aegujrzls7ds6uzvxwuekmez9cgzn3y7v6fd3ysjwttch"
     private let privateKeyHex = "5ead581a20d1db257728e4862fc3cd86b82619dccdb791170814e24f33496c49"
+    private let emptyString = ""
 
     func testPrivateKeyNsec() throws {
         let privateKey = try XCTUnwrap(PrivateKey(nsec: privateKeyNsec))
@@ -37,6 +38,20 @@ final class KeysTests: XCTestCase {
         let publicKey = try XCTUnwrap(PublicKey(hex: publicKeyHex))
         XCTAssertEqual(publicKey.npub, publicKeyNpub)
         XCTAssertEqual(publicKey.hex, publicKeyHex)
+    }
+
+    func testEmptyString() throws {
+        let publicKeyHex = PublicKey(hex: emptyString)
+        XCTAssertNil(publicKeyHex)
+
+        let publicKeyNpub = PublicKey(npub: emptyString)
+        XCTAssertNil(publicKeyNpub)
+
+        let privateKeyHex = PrivateKey(hex: emptyString)
+        XCTAssertNil(privateKeyHex)
+
+        let privateKeyNsec = PrivateKey(nsec: emptyString)
+        XCTAssertNil(privateKeyNsec)
     }
 
     func testKeypair() throws {
