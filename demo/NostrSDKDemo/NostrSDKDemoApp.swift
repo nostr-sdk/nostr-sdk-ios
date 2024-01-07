fb4c35cdffa85ca21580cb0de698d8414728427a
Bryan Montz <bryan.montz@lifechurch.tv>
2024-01-07 07:17:47 -0600

    update Demo app to support RelayPool

--- demo/NostrSDKDemo/NostrSDKDemoApp.swift
+++ demo/NostrSDKDemo/NostrSDKDemoApp.swift
@@ -5,13 +5,21 @@
 //  Created by Honk on 6/10/23.
 //
 
+import NostrSDK
 import SwiftUI
 
 @main
 struct NostrSDKDemoApp: App {
+    @StateObject var relayPool = try! RelayPool(relayURLs: [
+        URL(string: "wss://relay.damus.io")!,
+        URL(string: "wss://relay.snort.social")!,
+        URL(string: "wss://nos.lol")!
+    ])
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(relayPool)
         }
     }
 }
