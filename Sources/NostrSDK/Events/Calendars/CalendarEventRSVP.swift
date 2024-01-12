69c0420b3ea270719c0a385edc01a1a63b3b306a
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-01-11 20:35:17 -0500

    Remove label cruft in NIP-52 to reflect changes to spec

--- Sources/NostrSDK/Events/Calendars/CalendarEventRSVP.swift
+++ Sources/NostrSDK/Events/Calendars/CalendarEventRSVP.swift
@@ -33,20 +33,20 @@ public final class CalendarEventRSVP: NostrEvent, ParameterizedReplaceableEvent 
     /// The attendance status to the referenced calendar event.
     ///  Mimics the Participation Status type in the [RFC 5545 iCalendar spec](https://datatracker.ietf.org/doc/html/rfc5545#section-3.2.12).
     public var status: CalendarEventRSVPStatus? {
-        guard let statusTag = tags.first(where: { $0.name == TagName.label.rawValue && $0.otherParameters.first == "status" }) else {
+        guard let status = firstValueForRawTagName("status") else {
             return nil
         }
 
-        return CalendarEventRSVPStatus(rawValue: statusTag.value)
+        return CalendarEventRSVPStatus(rawValue: status)
     }
 
     /// Whether the user is free or busy for the duration of the calendar event.
     /// Mimics the Free/Busy Time Type in the [RFC 5545 iCalendar spec](https://datatracker.ietf.org/doc/html/rfc5545#section-3.2.9).
     public var freebusy: CalendarEventRSVPFreebusy? {
-        guard let freebusyTag = tags.first(where: { $0.name == TagName.label.rawValue && $0.otherParameters.first == "freebusy" }) else {
+        guard let freebusy = firstValueForRawTagName("fb") else {
             return nil
         }
 
-        return CalendarEventRSVPFreebusy(rawValue: freebusyTag.value)
+        return CalendarEventRSVPFreebusy(rawValue: freebusy)
     }
 }
