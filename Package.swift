abb9e73ab52b1c100043059d76e06378a71c0d11
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-04-04 23:28:21 -0400

    Replace Sodium with CryptoSwift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", from: "0.12.2"),
-        .package(url: "https://github.com/jedisct1/swift-sodium.git", revision: "63240810df971557fe9badc557257bdfbfeb90a3")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +26,7 @@ let package = Package(
             name: "NostrSDK",
             dependencies: [
                 .product(name: "secp256k1", package: "secp256k1.swift"),
-                .product(name: "Clibsodium", package: "swift-sodium")
+                "CryptoSwift"
             ]
         ),
         .testTarget(
